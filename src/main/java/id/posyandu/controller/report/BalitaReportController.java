f839c4ea29964b805af09ba47454e4750d29cca7
ikhsan nugraha ash shofar <shofar.ikhsan@gmail.com>
2016-08-24 20:55:48 +0700

    progress report balita jasper

--- src/main/java/id/posyandu/controller/report/BalitaReportController.java
+++ src/main/java/id/posyandu/controller/report/BalitaReportController.java
@@ -1,6 +1,14 @@
 package id.posyandu.controller.report;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,7 +18,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 
 import id.posyandu.service.BalitaService;
-import id.posyandu.domain.Balita;
+import id.posyandu.domain.ReportBalita;
 
 @Controller
 @RequestMapping("/report")
@@ -19,11 +27,33 @@ public class BalitaReportController {
 	@Autowired
 	BalitaService balitaService;
 	
+	@Autowired
+	DataSource ds;
+	
 	@RequestMapping("/balita")
     public ModelAndView generateReportMateri(ModelAndView m, 
-            @RequestParam(value = "format", required = false) String format){
-        Iterable<Balita> data = balitaService.getAllBalitas();
-        m.addObject("dataSource", data);
+            @RequestParam(value = "format", required = false) String format) throws SQLException{
+                
+        Connection c = ds.getConnection();
+		Statement stmt =c.createStatement();
+		String query = "SELECT b.nama, b.jenis_kelamin, b.tempat_lahir, b.tanggal_lahir, u.nama from balita b JOIN user u on u.user_id = b.ibu_userId";
+		ResultSet rs = stmt.executeQuery(query);
+		
+		List<ReportBalita> reportBalitas = new ArrayList<ReportBalita>();
+		while (rs.next()) {
+	         
+			 ReportBalita reportBalita = new ReportBalita();
+			 reportBalita.setNama(rs.getString("b.nama"));
+			 reportBalita.setJenisKelamin(rs.getString("b.jenis_kelamin"));
+			 reportBalita.setTempatLahir(rs.getString("b.tempat_lahir"));
+			 reportBalita.setTanggalLahir(rs.getDate("b.tanggal_lahir"));
+			 reportBalita.setIbu(rs.getString("u.nama"));
+	         
+	         reportBalitas.add(reportBalita);
+	         
+		}
+        
+        m.addObject("dataSource", reportBalitas);
         m.addObject("tanggalUpdate", new Date());
         m.addObject("format", "pdf");
         
@@ -31,7 +61,7 @@ public class BalitaReportController {
             m.addObject("format", format);
         }
         
-        m.setViewName("report_balita");
+        m.setViewName("report_balita3");
         return m;
     }
 	
---
+++ src/main/java/id/posyandu/domain/ReportBalita.java
@@ -0,0 +1,46 @@
+package id.posyandu.domain;
+
+import java.util.Date;
+
+public class ReportBalita {
+	String nama;
+	String tempatLahir;
+	Date tanggalLahir;
+	String jenisKelamin;
+	String ibu;
+	
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getTempatLahir() {
+		return tempatLahir;
+	}
+	public void setTempatLahir(String tempatLahir) {
+		this.tempatLahir = tempatLahir;
+	}
+	public Date getTanggalLahir() {
+		return tanggalLahir;
+	}
+	public void setTanggalLahir(Date tanggalLahir) {
+		this.tanggalLahir = tanggalLahir;
+	}
+	public String getJenisKelamin() {
+		return jenisKelamin;
+	}
+	public void setJenisKelamin(String jenisKelamin) {
+		this.jenisKelamin = jenisKelamin;
+	}
+	
+	public String getIbu() {
+		return ibu;
+	}
+	public void setIbu(String ibu) {
+		this.ibu = ibu;
+	}
+	
+	
+
+}
